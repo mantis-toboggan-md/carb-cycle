0b90018c198606cb5a3941d3630d9675aca78f91
Nancy <butlern0513@gmail.com>
2019-01-29 14:09:45 -0700

    changed favicon

--- API/server.js
+++ API/server.js
@@ -5,10 +5,13 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 const cowsay = require('cowsay')
 const chalk = require('chalk')
+const favicon = require('express-favicon');
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "../dist")));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 require("./config/session.js")(app);  ``
 var routes_setter = require('./config/routes.js');
